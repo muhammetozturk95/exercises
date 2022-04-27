25a889fed28a74b7a7c3c6fcb70c4cfb62288c55
Muhammet Öztürk <mhmozturk7@gmail.com>
2022-04-27 11:53:17 +0300

    completed

---
+++ my-module.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export async function getData(number) {
+    const {data: user} = await axios(`https://jsonplaceholder.typicode.com/users/${number}`);
+    const {data: post} = await axios(`https://jsonplaceholder.typicode.com/posts?id=${number}`);
+
+    console.log("user", user);
+    console.log("post", post);
+}
